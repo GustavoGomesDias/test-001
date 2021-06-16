a2211c6045818996d038c9abb5519f693c60c740
GustavoGomesDias <treeelevengg@gmail.com>
2021-06-15 21:42:10 -0300

    feat: add project basics configs

---
+++ src/app.js
@@ -0,0 +1,33 @@
+/* eslint-disable import/first */
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import { resolve } from 'path';
+
+const app = express();
+
+const whiteList = [
+  'http://localhost:4444',
+];
+
+const corsOptions = {
+  origin(origin, callback) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
+app.use(helmet());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(resolve(__dirname, 'uploads')));
+
+export default app;
---
+++ src/server.js
@@ -0,0 +1,8 @@
+import app from './app';
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running at ${port}`);
+  console.log(`CTRL + click in http://localhost:${port}`);
+});
