a100538e07d8b660fefef06834ff66d17e08c073
GustavoGomesDias <treeelevengg@gmail.com>
2021-06-16 01:36:42 -0300

    feat: add sales migrate

---
+++ src/database/migrations/20210616032937-sales.js
@@ -0,0 +1,36 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('sales', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+
+      value: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+      },
+
+      commission: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+      },
+
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('sales');
+  },
+};
