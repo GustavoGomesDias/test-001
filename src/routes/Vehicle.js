12391454cd35e228127ccbb3f657da6be6b18164
GustavoGomesDias <treeelevengg@gmail.com>
2021-06-17 00:41:48 -0300

    feat: add Vehicle routes

---
+++ src/routes/Vehicle.js
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import Vehicle from '../controllers/Vehicle';
+
+const router = Router();
+
+router.get('/vehicle', Vehicle.findAll);
+router.get('/vehicle/available', Vehicle.findByAvailable);
+router.post('/vehicle', Vehicle.create);
+router.put('/vehicle/edit/:chassis', Vehicle.edit);
+router.delete('/vehicle/delete/:chassis', Vehicle.delete);
+
+export default router;
