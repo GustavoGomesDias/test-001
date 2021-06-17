5aa33747ff19a6a27c546adf0936f9557b1350d5
GustavoGomesDias <treeelevengg@gmail.com>
2021-06-17 00:12:08 -0300

    feat: add vehicle controller

---
+++ src/controllers/Vehicle.js
@@ -0,0 +1,71 @@
+import VehicleModel from '../models/Vehicle';
+
+class Vehicle {
+  async findAll(req, res) {
+    try {
+      const vehicles = await VehicleModel.findAll();
+      return res.status(200).json(vehicles);
+    } catch (err) {
+      return res.status(500).json({ message: 'Erro no servidor, tente novamente mais tarde.' });
+    }
+  }
+
+  async findByAvailable(req, res) {
+    try {
+      const vehicles = await VehicleModel.findAll({ available: true });
+      return res.status(200).json(vehicles);
+    } catch (err) {
+      return res.status(500).json({ message: 'Erro no servidor, tente novamente mais tarde.' });
+    }
+  }
+
+  async create(req, res) {
+    try {
+      const vehicle = await VehicleModel.create(req.body);
+      return res.status(200).json(vehicle);
+    } catch (err) {
+      return res.status(400).json({
+        errors: err.errors.map((e) => e.message),
+      });
+    }
+  }
+
+  async edit(req, res) {
+    try {
+      const { chassis } = req.params;
+      const vehicle = await VehicleModel.findByPk(chassis);
+
+      if (!vehicle) {
+        return res.status(401).json({ message: 'Veículo não existe.' });
+      }
+
+      const newVehicle = await vehicle.update(req.body);
+      return res.status(200).json(newVehicle);
+    } catch (err) {
+      return res.status(400).json({
+        errors: err.errors.map((e) => e.message),
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const vehicle = await VehicleModel.findByPk(req.params.chassis);
+
+      if (!vehicle) {
+        return res.status(401).json({
+          message: 'Veículo não existe.',
+        });
+      }
+
+      await vehicle.destroy();
+      return res.json({ message: 'Veículo deletado.' });
+    } catch (err) {
+      return res.status(400).json({
+        errors: err.errors.map((e) => e.message),
+      });
+    }
+  }
+}
+
+export default new Vehicle();
