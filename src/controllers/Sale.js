9c2548661822abc7013b7e7365d7b318d412796a
GustavoGomesDias <treeelevengg@gmail.com>
2021-06-21 22:45:37 -0300

    feat: ad find by chassis and get acquisitions values from sales listing

--- src/controllers/Sale.js
+++ src/controllers/Sale.js
@@ -1,22 +1,32 @@
 import SaleModel from '../models/Sale';
+import AcquisitionModel from '../models/Acquisition';
 
 class Sale {
   async findAll(req, res) {
     try {
-      const sales = await SaleModel.findAll();
+      const sales = await SaleModel.findAll({
+        include: AcquisitionModel,
+      });
       return res.status(200).json(sales);
     } catch (err) {
+      console.log(err);
       return res.status(500).json({ message: 'Erro no servidor, tente novamente mais tarde.' });
     }
   }
 
   async findByChassis(req, res) {
     try {
-      const { acquisition_id } = req.params;
-      const sale = await SaleModel.findAll({ where: { acquisition_id } });
+      const { chassis } = req.params;
+      const sale = await SaleModel.findAll({
+        include: [{
+          model: AcquisitionModel,
+          where: { chassis },
+        }],
+      });
 
       return res.status(200).json(sale[0]);
     } catch (err) {
+      console.log(err);
       return res.status(500).json({ message: 'Erro no servidor, tente novamente mais tarde.' });
     }
   }
@@ -32,6 +42,8 @@ class Sale {
 
       const commission = parseFloat(value) * 0.1;
       const sale = await SaleModel.create({ value, commission, acquisition_id });
+      const acquisisiton = await AcquisitionModel.findByPk(acquisition_id);
+      await acquisisiton.update({ available: false });
       return res.status(200).json(sale);
     } catch (err) {
       console.log(err);
@@ -48,10 +60,10 @@ class Sale {
       if (!sale) {
         return res.status(401).json({ message: 'Venda não encontrada.' });
       }
-      const { value, chassis } = req.body;
+      const { value, acquisition_id } = req.body;
       const commission = parseFloat(value) * 0.1;
 
-      const newSale = await sale.update({ value, commission, chassis });
+      const newSale = await sale.update({ value, commission, acquisition_id });
       return res.status(200).json(newSale);
     } catch (err) {
       console.log(err);
